f2b9bb1b58e9ff2085c8b80609ab81bb4e59f97a
Eric Peterson <eaptrsn@gmail.com>
2025-09-14 15:40:48 -0500

    arbitrary change to get heroku to redeploy with new buildpack settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "books#index"
-
+  # arbitrary change to get heroku to redeploy with new buildpack settings
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
